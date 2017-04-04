02d7177d544c83f16c53ebec820314d996183dba
Julie S Mike <juliesmike@gmail.com>
2017-04-04 14:28:07 -0500

    readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Check Box JavaScript
+
+30 Day Code Challenge Project
+
+- Initial commit is starter code provided by the JavaScript 30 Coding challenge https://javascript30.com
+- Check boxes in between when hold down shift key
--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-window.addEventListener('load', function(event){
+window.addEventListener('load', function(event) {
 
-  const boxes =  document.querySelectorAll('.inbox input[type="checkbox"]');
+    const boxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
-let lastChecked;
+    let lastChecked;
 
-function isChecked(e){
-  //is shift key down and are checking
-  let inBetween = false;
+    function isChecked(e) {
+        //is shift key down and are checking
+        let inBetween = false;
 
-  if(e.shiftKey && this.checked){
-    //loop thru checkboxes, look for first checked and check inbetween
-    boxes.forEach(checkbox => {
-      console.log(checkbox);
-      if(checkbox === this || checkbox === lastChecked){
-        inBetween = !inBetween;
-        console.log('start checking/end checking');
-      }
-      if(inBetween){
-        checkbox.checked = true;
-      }
-    });
-  }
-  lastChecked = this;
+        if (e.shiftKey && this.checked) {
+            //loop thru checkboxes, look for first checked and check inbetween
+            boxes.forEach(checkbox => {
+                console.log(checkbox);
+                if (checkbox === this || checkbox === lastChecked) {
+                    inBetween = !inBetween;
+                    console.log('start checking/end checking');
+                }
+                if (inBetween) {
+                    checkbox.checked = true;
+                }
+            });
+        }
+        lastChecked = this;
 
-  console.log(lastChecked);
-}
+        console.log(lastChecked);
+    }
 
-  boxes.forEach(checkbox => checkbox.addEventListener('click', isChecked));
+    boxes.forEach(checkbox => checkbox.addEventListener('click', isChecked));
 
 });
